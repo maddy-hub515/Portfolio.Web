4b0bd31b04b3981b8bc33e131696f9895acf6e0c
JANA .R <madeshram66@gmail.com>
2026-08-29 17:58:07 +0530

    Removed back slash

--- backend/MR.Portfolio-Api/Program.cs
+++ backend/MR.Portfolio-Api/Program.cs
@@ -14,7 +14,7 @@ builder.Services.AddCors(options =>
     options.AddPolicy("AllowReactApp", policy =>
     {
         policy
-            .WithOrigins("http://localhost:5173", "https://maddys-portfolio.netlify.app/")
+            .WithOrigins("http://localhost:5173", "https://maddys-portfolio.netlify.app")
             .AllowAnyHeader()
             .AllowAnyMethod();
     });
